da28dd61a198b6dfdc132bd5184a69cff33dfa48
BABA Aristote Cleven <babaaristote6@gmail.com>
2026-09-01 00:46:58 +0100

    ok

--- cvbuilder/app/components/cv-templates/CVMinimal.tsx
+++ cvbuilder/app/components/cv-templates/CVMinimal.tsx
@@ -129,7 +129,6 @@ const CVMinimal: React.FC<Props> = ({
                 {pd.description}
               </p>
             </section>
-            
           )}
           {display.skills.length > 0 && (
             <section>
